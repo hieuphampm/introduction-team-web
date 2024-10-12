2f683a7050cd2e005bd2f0cdc3e59552c349d53d
Phuoc Hieu <hieuphamminh976@gmail.com>
2024-10-12 10:49:31 +0700

    update

--- introduction-team/src/App.js
+++ introduction-team/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home'
 import Teaminfo from './components/Teaminfo'
+import HieuInfo from './components/teaminfo/HieuInfo'
 
 export class App extends Component {
   render(){
@@ -9,7 +10,8 @@ export class App extends Component {
       <BrowserRouter>
         <Routes>
            <Route exact path="/" element={<Home />} />
-           <Route exact path="Tea minfo" element={<Teaminfo />} />
+           <Route exact path="Teaminfo" element={<Teaminfo />} />
+           <Route exact path='HieuInfo' element={<HieuInfo />} />
         </Routes> 
       </BrowserRouter>
     );
--- introduction-team/src/components/Teaminfo.js
+++ introduction-team/src/components/Teaminfo.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import '../css/Teaminfo.css'
 
 const Teaminfo = () => {
   return (
-    <div>
-      
+    <div className='row'>
+      <div className="col-sm-6 mb-3 mb-sm-0">
+        <div className="card">
+          <div className="card-body">
+            <h4 className="card-title">Phạm Phước Minh Hiếu</h4>
+            <p className="card-text">The leader of HBK Team</p>
+            <Link to="/HieuInfo"> 
+              <button type="button" className="btn btn-custom btn-lg">About Hieu</button>
+            </Link>   
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
